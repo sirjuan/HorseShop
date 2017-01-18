cbb6bcf484a45eaf8b41ab739e09b79ada61f4c1
sirjuan <t6peju01@students.oamk.fi>
2017-01-18 10:41:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/horseshop/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.horseshop;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
